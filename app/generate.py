8f0dbe3b34641c2085f1efc2a3d69410a58fb3c2
michael <michael@not.here.com>
2020-05-20 19:43:03 +0200

    autocommit

--- app/generate.py
+++ app/generate.py
@@ -10,6 +10,8 @@ import json
 from pathlib import Path
 from git import Repo
 from znode import *
+import znode
+from znode import *
 import cirkel
 
 def do_git():
@@ -28,12 +30,6 @@ def do_git():
         return str(repo.head.commit)    
     return { 'cirkel':check_commit(git_path1), 'znode':check_commit(git_path2) }
 
-print("-------------------------------------------------------------------")
-commits = do_git()  
-print("-------------------------------------------------------------------")
-
-print(commits)
-xxx
 
 
 ##############################################################################################################################
@@ -73,14 +69,13 @@ class layered_saver:
 #####################################################################################################    
 
 def set_image_data(im, data):
+    data = json.dumps(data)
     im = exiv2.ImageFactory.open(im)
     im.readMetadata()
 
     new_iptc_data = exiv2.IptcData()
     new_iptc_data[b"Iptc.Application2.Caption"] = data
     im.setIptcData(new_iptc_data)
-    im.clearExifData()
-    im.clearXmpData()
     im.writeMetadata()
 
     io = im.io()
@@ -92,7 +87,10 @@ def read_metadata(path):
     image = exiv2.ImageFactory.open(str(path))
     image.readMetadata()
     ipc_data = image.iptcData()
-    return ipc_data[b"Iptc.Application2.Caption"].toString().decode('ascii')
+    jsondata = ipc_data[b"Iptc.Application2.Caption"].toString().decode('ascii')
+    data = json.loads(jsondata)
+    return data
+    #return znode_load(data['graph'])
     
 def read_folder(path, shuffle = True):
     files = list(path.rglob("*.jpg"))
@@ -107,7 +105,7 @@ def save_image(L, image, graph):
     data = graph.dump()
     data = {
         'info' : {
-            
+            g_commits
         },
         'graph' : data
     }
@@ -190,6 +188,72 @@ def create_graph(seed, seed2):
         
     return n_all
 
+def create_graph(seed):
+    nrnd = ŋnp_RandomState(seed)
+    
+    # An array of start points (x, y, angle). 64 points in this example.
+    i = 8
+    nxy = ŋnp_transpose(ŋnp_indices((i,i)), (1,2,0))
+    nxy = (1/i)*(nxy+0.5)
+    nxy = ŋnp_reshape(nxy, (i*i,2))
+        
+    na = ŋrandom(nrnd, (i*i,1)) / 10
+    S = ŋnp_concatenate((nxy, na), 1)
+    S = ŋnp_ascontiguousarray(S)
+        
+    # 2d-array of "instructions", either 0 or 1    
+    J = ŋrandint(nrnd, 0, 2, (2,2))
+ 
+    # 2d-array of rotation angles in radians. Can be negative.
+    R = ŋrandint(nrnd,0,8,(4,4))*np.pi/4
+
+    # 2d-array of diameters (integers > 0)
+    D = ŋnp_array((
+        (50,60,50,50),
+        (20,50,10,15)
+    ))
+    
+    # branch array, also angles
+    B = ŋrandint(nrnd,0,8,(2,4))*np.pi/2
+    
+    B = B.slice_multiply[0,0:1,0.98]
+    B = B.slice_multiply[1,2:3,1.02]
+    
+        
+    # create 3072x3072 picture
+    picture_output_size = 3072
+    
+    # max depth of the stack
+    stack_size = 6
+    
+    # param1, integer
+    param1 = 1
+
+    # param2, integer
+    param2 = 20
+        
+    # Colors of the disks as (r,g,b)
+    # Number of colors must be equal to stack size or bad things happen!
+    RGB = (ŋrandom(nrnd, (stack_size, 3))*256).astype(np.uint8)
+
+    # number of RGB_INCREMENT must be equal to stack size!
+    RGB_INCREMENT = (ŋrandn(nrnd, stack_size, 3)*10).astype(np.uint8)
+
+    r_background = 0
+    g_background = 0
+    b_background = 23
+    
+    n_all = ŋtuple(
+        S, J, R, D, B,
+        picture_output_size, 
+        stack_size, 
+        param1, param2,
+        RGB, RGB_INCREMENT, r_background, g_background, b_background
+    )
+    
+    n_all.eval()
+    return n_all
+
 ######################################################################################################
 
 def vary_folder():       
@@ -197,12 +261,12 @@ def vary_folder():
     L = layered_saver(r"F:\GEN\CIRKEL", 300)
     for x in read_folder(folder, False):
         print(x)
-        graph = read_metadata(x)
-        graph = json_loads(graph)
-        #print(graph)
-        nrnd = graph.find_first_of_type(ŋnp_RandomState)
-        print(nrnd[0].r)
-        graph2 = create_graph(nrnd[0].r, random.randint(0,1000))
+        data = read_metadata(x)
+        graph = znode.load(data)
+        print(graph)
+        #nrnd = graph.find_first_of_type(ŋnp_RandomState)
+        #print(nrnd[0].r)
+        #graph2 = create_graph(nrnd[0].r, random.randint(0,1000))
         '''
         graph2.eval()
         image = cirkel.cirkel2(*graph2.r)
@@ -220,9 +284,12 @@ def generate_new():
         image = cirkel.cirkel2(*graph.r)
         save_image(L, image, graph) 
 
+if __name__ == '__main__':
+    print("-------------------------------------------------------------------")
+    g_commits = do_git()  
+    print("-------------------------------------------------------------------")
 
-        
-vary_folder()
+    vary_folder()
 
 
 
