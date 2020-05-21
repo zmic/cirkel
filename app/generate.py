115cb1fb32b1094b73b9918ca4660776ebf1acb5
michael <michael@not.here.com>
2020-05-21 02:36:30 +0200

    autocommit

--- app/generate.py
+++ app/generate.py
@@ -181,9 +181,9 @@ if __name__ == '__main__':
     L = layered_saver(root_folder / "GEN", 300)
     
     for i in range(500):
-        vary_folder(L, good_folder)
+        #vary_folder(L, good_folder)
         #clone_folder(L, good_folder)
-        #create_new_cirkel2(L)
+        create_new_cirkel2(L)
 
 
 
